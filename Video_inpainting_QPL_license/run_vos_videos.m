58c813670d00330e02c1b6cc6d24a5dda4538bcc
Zhe-Yu Liu <j2325138@gmail.com>
2019-01-05 16:40:36 +0800

    Modify run script for patch 3

--- Video_inpainting_QPL_license/run_vos_videos.m
+++ Video_inpainting_QPL_license/run_vos_videos.m
@@ -1,26 +1,28 @@
 function[outputPath] = run_vos_videos(start_i, end_i)
-
-    video_dirs = dir('../../VOS_resized2/synthesized_frames/bg_*');
     
+    root_dir = '../../VideoObjectRemoval/VOS_origin';
+    video_dirs = dir(fullfile(root_dir, 'synthesized_frames/bg_*'));
+
     for i = start_i : end_i
         video_name = sprintf('%s', video_dirs(i).name);
         
         video_dir = fullfile(video_dirs(i).folder, video_name);
-        mask_dir = fullfile('../../VOS_resized2/fg_segm_bbox_masks', video_name);
+        mask_dir = fullfile(root_dir, 'fg_segm_bbox_masks', video_name);
         
         if length(dir(fullfile(video_dir, '*'))) ~= length(dir(fullfile(mask_dir, '*')))
-            %disp(sprintf('Video %s has different video length and mask length', video_name))
+            disp(sprintf('Video %s has different video length and mask length', video_name))
+            disp('Insufficient part will be padded with previous frame/mask')
             %continue
         end
         
-        if exist(fullfile('.', 'VOS_output', video_name))
+        if exist(fullfile('.', 'VOS_origin_output_patch_3', video_name))
             disp(sprintf('Video %s done before, skip', video_name))
             continue
         end
         
         imgVolOut = start_inpaint_video_mod(video_dir, mask_dir, -1, -1);
         
-        cd VOS_output
+        cd VOS_origin_output_patch_3
         mkdir(video_name)
         cd(video_name)
         write_image_volume(imgVolOut, '');
